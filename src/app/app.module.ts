067361aa5644d14c3088735e3cd0c91bde22aaf9
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-17 16:08:07 +0100

    import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleModule } from '@carsten-leue/ivy-module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SampleModule,
     BrowserModule,
     AppRoutingModule
   ],
